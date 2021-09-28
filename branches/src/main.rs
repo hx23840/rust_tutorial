45beeef73319a5ac7758066fc84a7a8a8e893bc4
Yoshi <rabbit20110410@gmail.com>
2021-09-28 17:30:06 +0800

    控制流

---
+++ branches/src/main.rs
@@ -0,0 +1,69 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let number = if number < 5 {
+        5
+    } else {
+        10
+    };
+
+    println!("The value of the number is {}", number);
+
+    let mut count = 0;
+
+    'continue_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+
+            if count == 2 {
+                break 'continue_up;
+            }
+
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("End of count = {}", count);
+
+    let result = loop {
+        count += 1;
+
+        if count == 10 {
+            break count * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    while count != 0 {
+        println!("{}", count);
+
+        count -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("The value of the element is {}", element);
+    };
+
+    for number in (1..10).rev() {
+        println!("{}", number);
+    }
+}
