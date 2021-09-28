3120a07489d842b13d94c458dd15d6f0e7c5fef8
Yoshi <rabbit20110410@gmail.com>
2021-09-28 14:18:20 +0800

    增加基础数据类型的使用

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,4 +10,13 @@ fn main() {
     }
 
     println!("The value of the x is {}", x);
+
+    let heart_eyed_cat = '😻';
+    println!("{}", heart_eyed_cat);
+
+    let tup = (500, 6.4, 1);
+    println!("{} {} {}", tup.0, tup.1, tup.2);
+
+    let arr = [3; 5];
+    println!("{}", arr[4]);
 }
